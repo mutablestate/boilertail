e398d167583fb006495af020ea6a96f533b8a8ee
Ben <benwebdev@gmail.com>
2019-06-03 07:48:37 +0700

    Add tailwindcss
    * remove `phoenix.css`
    * add `tailwindcss` and `postcss-loader`
    * configure `postcss` loader for `webpack`
    * generate `tailwind.config.js` and test with extended colors in `eex`

---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        elixir: '#4e2a8e',
+        phoenix: '#f67938'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env, options) => ({
     ]
   },
   entry: {
-      './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
+    './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
   },
   output: {
     filename: 'app.js',
@@ -30,7 +30,17 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [require('tailwindcss'), require('autoprefixer')]
+            }
+          }
+        ]
       }
     ]
   },
