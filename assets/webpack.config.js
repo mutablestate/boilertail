854b2dbde8e00af95c42d648c76081f8dfcdc0f3
Ben <benwebdev@gmail.com>
2019-06-04 07:20:06 +0700

    Style starter page
    * add `postcss-import`
    * fix postcss config
    * organise css imports / files
    * example tailwindcss usage for starter page

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -30,17 +30,7 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: [require('tailwindcss'), require('autoprefixer')]
-            }
-          }
-        ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       }
     ]
   },
