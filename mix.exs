5ebb1a95eb80bc0ca1bb7bdb32c93b67579a4e43
Ben Sharman <benwebdev@gmail.com>
2020-02-08 17:57:25 +0700

    Hex package updates

--- mix.exs
+++ mix.exs
@@ -33,16 +33,16 @@ defmodule Boilertail.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.6"},
+      {:phoenix, "~> 1.4.12"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.0"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:ecto_sql, "~> 3.3"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 2.14"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:gettext, "~> 0.17"},
+      {:jason, "~> 1.1"},
+      {:plug_cowboy, "~> 2.1"}
     ]
   end
 
